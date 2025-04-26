d49ad40adb7ee9cf1fdf6192b084f87caf693fc0
Rb-112594 <reboureslan@gmail.com>
2025-04-26 13:36:57 +0300

    Starting the Dom Manipulation of authentication page.

---
+++ scripts/auth.js
@@ -0,0 +1,14 @@
+class AuthSystem {
+  constructor() {
+    this.domElements = {
+      loginTabBtn: document.getElementById("loginTabBtn"),
+      signupTabBtn: document.getElementById("signupTabBtn"),
+      loginSection: document.getElementById("loginSection"),
+      resetPassSection: document.getElementById("resetPassSection"),
+      signupSection: document.getElementById("signupSection"),
+      forgotPasswordLink: document.getElementById("forgotPasswordLink"),
+      loginForm: document.getElementById("loginForm"),
+      signupForm: document.getElementById("signupForm"),
+    };
+  }
+}
