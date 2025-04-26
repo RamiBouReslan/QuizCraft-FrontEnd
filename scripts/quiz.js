5e01eb30bbc0e588ae1787d461f8fecb19cce5f9
Rb-112594 <reboureslan@gmail.com>
2025-04-26 15:45:45 +0300

    Including Score function eventlisteners

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -39,4 +39,37 @@ const elements = {
     elements.quizForm.innerHTML = questionsHTML; 
   }
   
- 
+  function handleQuizSubmission() {
+    let score = 0;
+  
+    currentQuiz.questions.forEach((question, index) => {
+      const selectedOption = document.querySelector(`input[name="question_${index}"]:checked`);
+      
+      if (selectedOption && selectedOption.nextElementSibling.textContent.trim() === question.answer) {
+        score++;
+      }
+    });
+  
+    elements.scoreFooter.innerHTML = `You Scored: ${score} / ${currentQuiz.questions.length}`;
+  }
+  
+  
+  function setupEventListeners() {
+    elements.submitButton.addEventListener("click", handleQuizSubmission);
+    
+    elements.homeButton.addEventListener("click", function() {
+      window.location.href = "../pages/home.html";  
+    });
+    
+    elements.logoutButton.addEventListener("click", function() {
+      sessionStorage.removeItem("currentUser");
+      window.location.href = "../index.html";  
+    });
+  }
+  
+
+  document.addEventListener("DOMContentLoaded", function() {
+    initializeQuiz();  
+    setupEventListeners();  
+  });
+  
